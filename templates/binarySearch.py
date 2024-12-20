a4f84be158d20fdeef3c753acc6fdaa7cbaff873
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2024-12-19 23:11:26 -0500

    implemented binary search

--- templates/binarySearch.py
+++ templates/binarySearch.py
@@ -1,3 +1,4 @@
+from token import ELLIPSIS
 from utils.timing_utils import measure_time
 
 '''
@@ -10,6 +11,25 @@ def linear_search(arr: list[int], query: int) -> int:
     for index, value in enumerate(arr):
         if value == query:
             return index
+    return -1
+
+def binary_search(arr: list[int], query: int) -> int:
+    # empty array -> return -1 right away (small obvious optimization)
+    if len(arr) == 0:
+        return -1
+    
+    left: int = 0
+    right: int = len(arr) - 1
+    
+    while left <= right:
+        mid: int = int((left + right) / 2)
+        if arr[mid] == query:
+            return mid
+        elif arr[mid] > query:
+            right = mid - 1
+        else:
+            left = mid + 1
+    # if we havent returned index so far then no match was found
     return -1
 
 
