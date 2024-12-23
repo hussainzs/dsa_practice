40e661f7dc7603b93a05a403dde0a0c560972e9f
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2024-12-23 00:44:34 -0500

    implemented add get_index method and made Node class methods public

--- templates/doublyLinkedList.py
+++ templates/doublyLinkedList.py
@@ -11,25 +11,25 @@ class Node:
         self.next: Optional['Node'] = next
         
     # define getters 
-    def _getValue(self) -> Any:
+    def getValue(self) -> Any:
         return self.value
     
-    def _getPrev(self) -> Any:
+    def getPrev(self) -> Optional['Node']:
         return self.prev
 
-    def _getNext(self) -> Any:
+    def getNext(self) -> Optional['Node']:
         return self.next
     
     #-------
     
     #define setters
-    def _setValue(self, newValue: Any) -> None:
+    def setValue(self, newValue: Any) -> None:
         self.value = newValue
     
-    def _setPrev(self, newPrev: Optional['Node']) -> None:
+    def setPrev(self, newPrev: Optional['Node']) -> None:
         self.prev = newPrev
         
-    def _setNext(self, newNext: Optional['Node']) -> None:
+    def setNext(self, newNext: Optional['Node']) -> None:
         self.next = newNext
         
 
@@ -45,10 +45,24 @@ class DoubleLinkedList:
             self.append(c)
         
     def append(self, val: Any) -> None:
-        currentLast: Node = self.last
-        newLastNode: Node = Node(val, prev=currentLast, next=None)
-        self.last.next = newLastNode
-        self.last = newLastNode
+        currentLast: Node = self.last  # Store the current last node from the list
+        # Create a new node with the given value, setting its previous node to the current last node and next node to None
+        newLastNode: Node = Node(val, prev=currentLast, next=None)  
+        self.last.next = newLastNode  # Link the current last node next pointer to new last node
+        self.last = newLastNode  # Update the last node reference to the new node
+        self.length += 1  
+        
+    def get_index(self, index: int) -> Optional['Node']:
+        if index < 0 or index >= self.length:
+            max_valid_index: int = 0 if self.length == 0 else self.length - 1
+            raise IndexError(f"Index out of bounds, max valid index was {max_valid_index}")
+        
+        curr_node: Optional['Node'] = self.first.getNext()
+        for _ in range(index):
+            if curr_node is not None:
+                curr_node = curr_node.getNext()
+        return curr_node
+            
         
         
 
