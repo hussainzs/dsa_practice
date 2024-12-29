02d5585f84a94dd405e7d7aa651b944ae567dfe2
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2024-12-28 22:44:40 -0500

    initial Queue class structure with method signatures

---
+++ templates/queue.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+class Queue:
+    def __init__(self, contents: list[Any] | None = None) -> None:
+        pass
+    
+    def enqueue(self, item: Any) -> None:
+        pass
+    
+    def peek (self) -> Any:
+        pass
+    
+    def is_empty(self) -> bool:
+        return False
+    
+    def dequeue(self) -> Any:
+        pass
