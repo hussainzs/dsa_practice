13c13aa565770c38c12b63e83c6c1c9b5634e00a
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2024-12-28 22:20:49 -0500

    add initial Stack class with method signatures

---
+++ templates/stacks.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+class Stack:
+    def __init__(self, contents: list[Any] | None = None) -> None:
+        pass
+    
+    def push(self, item: Any) -> None:
+        pass
+    
+    def pop(self) -> Any:
+        pass
+    
+    def peek(self) -> Any:
+        pass
+    
+    def isEmpty(self) -> bool:
+        pass
+        
