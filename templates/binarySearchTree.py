5699e4ce6f276e5ce4025fe7d35dd4164743faa5
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2024-12-30 21:55:44 -0500

    enhance search method with detailed docstring and type check for None

--- templates/binarySearchTree.py
+++ templates/binarySearchTree.py
@@ -51,8 +51,21 @@ class BinarySearchTree:
             
 
     def search(self, val: int) -> Optional[BSTNode]:
+        """Finds and returns the Node that contains the desired value. Returns None if not found
+        In case of duplicates, it finds the first instance.
+
+        Args:
+            val (int): value to be found.
+
+        Returns:
+            Optional[BSTNode]: The node containing the value
+        """
+        if val is None:
+            raise TypeError("Can not search for None in BST")
+        
         if self.num_nodes == 0:
-            return None
+            return None # there is nothing to search in an empty BST
+        
         def __search_recursive(curr: Optional['BSTNode']) -> Optional[BSTNode]:
             if curr is None:
                 return None # couldn't find the desired node
