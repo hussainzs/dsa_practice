adeb2168b8f0029c6fbfc0525f8851c5d91fe5b9
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2024-12-30 21:00:42 -0500

    Implement insert method

--- templates/binarySearchTree.py
+++ templates/binarySearchTree.py
@@ -9,26 +9,47 @@ class BSTNode:
 
 
 class BinarySearchTree:
-    def __init__(self) -> None:
+    def __init__(self, contents: list[int] | None = None) -> None:
         self.root: Optional[BSTNode] = None
+        
+        #TODO: handle contents array
 
-    def insert(self, val: int) -> None:
-        pass
+    def insert(self, val: int) -> BSTNode:
+        """Inserts a integer into BST. 
+
+        Args:
+            val (int): integer to be inserted
+
+        Raises:
+            TypeError: Can not insert None values into BST
+
+        Returns:
+            BSTNode: Returns the root of the tree
+        """
+        if val is None:
+            raise TypeError("Can not insert None value into Binary Search Tree")
+        
+        # we will call this recursive method starting from root
+        def __insert_recursive(curr: Optional['BSTNode'], val: int) -> BSTNode:
+            # Base Case:
+            if curr is None:
+                return BSTNode(value=val, right=None, left=None)
+            if val >= curr.value:
+                curr.right = __insert_recursive(curr.right, val)
+            else:
+                # val < curr.value
+                curr.left = __insert_recursive(curr.left, val)
+            return curr
+            
+        self.root = __insert_recursive(self.root, val)
+        return self.root
+            
 
     def search(self, val: int) -> Optional[BSTNode]:
         pass
 
     def delete(self, val: int) -> None:
         pass
-
-    def inorder_traversal(self) -> list[int]:
-        return []
-
-    def preorder_traversal(self) -> list[int]:
-        return []
-
-    def postorder_traversal(self) -> list[int]:
-        return []
 
     def find_min(self) -> Optional[int]:
         pass
@@ -38,3 +59,12 @@ class BinarySearchTree:
 
     def is_empty(self) -> bool:
         return False
+    
+    def inorder_traversal(self) -> list[int]:
+        return []
+
+    def preorder_traversal(self) -> list[int]:
+        return []
+
+    def postorder_traversal(self) -> list[int]:
+        return []
