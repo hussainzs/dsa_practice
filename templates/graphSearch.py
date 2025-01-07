ed45f76f3853bc342d443d7f8c546746fc9292de
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2025-01-06 22:48:31 -0500

    initial bfs impl

--- templates/graphSearch.py
+++ templates/graphSearch.py
@@ -5,8 +5,28 @@ graph = list[list[int]] adjacency list
 i.e. = [[1,2,3], [5,6], ...]
 """
 
-def breath_first_search(graph: list[list[int]]):
-    pass
+def breath_first_search(graph: list[list[int]], start: int) -> list[int]:
+    """
+    Perform a breadth-first search (BFS) on the graph starting from the given node.
+
+    Args:
+        graph (dict[int, list[int]]): A dictionary where the key represents the node and the value represents its neighbors.
+        start (int): The starting node for the BFS.
+
+    Returns:
+        list[int]: A list of nodes in the order they were discovered during the BFS.
+    """
+    Q: deque[int] = deque([start])
+    discovered: list[int] = []
+    
+    while len(Q) != 0:
+        curr_parent: int = Q.popleft() # popLeft gets the node at the front of the queue
+        discovered.append(curr_parent)
+        for neighbor in (graph[curr_parent]):
+            if neighbor not in discovered:
+                Q.append(neighbor)
+                
+    return discovered
     
     
 def depth_first_search(graph: list[list[int]]) -> list[list[int]]:
