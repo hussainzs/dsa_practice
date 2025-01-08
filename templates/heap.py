7835903add67fdc83737b17a2c4ddaf781e55336
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2025-01-07 20:26:00 -0500

    heap class sketch

---
+++ templates/heap.py
@@ -0,0 +1,36 @@
+class Heap:
+    def __init__(self):
+        # Initialize the heap
+        self.heap = []
+
+    def insert(self, element):
+        # Insert an element into the heap
+        pass
+
+    def delete(self):
+        # Delete the root element from the heap
+        pass
+
+    def heapify_up(self, index):
+        # Heapify up to maintain heap property
+        pass
+
+    def heapify_down(self, index):
+        # Heapify down to maintain heap property
+        pass
+
+    def get_min(self):
+        # Get the minimum element (for min-heap)
+        pass
+
+    def get_max(self):
+        # Get the maximum element (for max-heap)
+        pass
+
+    def size(self):
+        # Return the size of the heap
+        pass
+
+    def is_empty(self):
+        # Check if the heap is empty
+        pass
