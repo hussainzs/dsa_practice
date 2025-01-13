78fabd7021d144582290b659c881a3497da21367
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2025-01-13 09:12:53 -0500

    fixed calculation of number of nodes and added kahns algorithm sudo code

--- templates/graphSearch.py
+++ templates/graphSearch.py
@@ -75,7 +75,7 @@ def depth_first_search(graph: list[list[int]]) -> list[list[int]]:
     return result
 
 
-def calculate_indegree_from_edgeList(edges_arr: list[tuple[int, int]]) -> list[int]:
+def calculate_indegree_from_edgeList(graph: list[tuple[int, int]]) -> list[int]:
     """
     You are given a graph with n nodes, where each node has an integer value from 0 to n - 1.
     The graph is represented by a list of edges, where edges[i] = [u, v] is a directed edge from node u to node v. 
@@ -91,16 +91,16 @@ def calculate_indegree_from_edgeList(edges_arr: list[tuple[int, int]]) -> list[i
     Returns:
         list[int]: indegree if each node
     """
-    n: int = len(edges_arr)
+    n: int = max(max(u, v) for u, v in graph) + 1
     indegree: list[int] = [0 for _ in range(n)] # populate n zeros
-    for src, dest in edges_arr:
+    for src, dest in graph:
         indegree[dest] += 1
     return indegree
     
 
 def calculate_indegree_from_adjList(adj_list: dict[int, list[int]]) -> list[int]:
     """ Calculate indegree of each node in adj list
-    Input: edges = {0: [1], 1: [2, 3], 2: [], 3: [2, 4], 4: []}
+    Input: adj_list = {0: [1], 1: [2, 3], 2: [], 3: [2, 4], 4: []}
     Output: [0, 1, 2, 1, 1]
 
     Args:
@@ -122,15 +122,36 @@ We maintain the indegree of each node (initially O(n) to find all in-degrees but
 Each time we remove a node from the graph, we decrement the indegree of all of its outneighbors. 
 If the indegree of any of these nodes becomes 0, then we add it to our list.
 '''
-def topological_sort(graph: list[list[int]]) -> list[int]:
-    """
-    Performs topological sort on a directed acyclic graph (DAG).
+def topological_sort(graph: dict[int, list[int]] | list[tuple[int, int]]) -> list[int]:
+    """Finding topological ordering of the given graph.
+    Input examples:
+    adj_list =  {0: [1], 1: [2, 3], 2: [], 3: [2, 4], 4: []}
+    edge list =  [(0, 1), (1, 2), (1, 3), (3, 2), (3, 4)]
 
     Args:
-        graph (List[List[int]]): Adjacency list representation of graph
-        where graph[i] contains neighbors of vertex i
+        graph (dict[int, list[int]] | list[tuple[int, int]]): either adjacency list or edge list
 
     Returns:
-        List[int]: A list of vertices in topologically sorted order.
+        list[int]: topological ordering - dependency perserving ordering of the vertices
     """
-    return []
+    
+    # The algorithm goes like this kahns toposort 
+    '''
+    1. Calculate the indegree of each node.
+    2. Add all nodes with an indegree of 0 to a queue.
+    3. While the queue is not empty:
+        4. Dequeue the first node from the queue and add it to the topological order.
+        5. For each neighbor of the node, decrement its indegree by 1. If the neighbor's indegree is now 0, add it to the queue.
+    Return the topological order.
+    '''
+    # claulcate indegree of each node
+    indegree: list[int] = []
+    if isinstance(graph, dict): # if we get adjacency list
+        indegree = calculate_indegree_from_adjList(graph)
+    elif isinstance(graph, list): # if we get edge list
+        indegree = calculate_indegree_from_edgeList(graph)
+    else:
+        raise TypeError("Input is not of the desired type")
+        
+        
+    
